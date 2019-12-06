ad37f1385578cd8d17060cb367bd879a88beaf30
Benjamin Avila Rosas <benjaavro@gmail.com>
2019-12-05 18:16:11 -0600

    Latest UI changes prior delivery

--- recruited_app/lib/new-post.dart
+++ recruited_app/lib/new-post.dart
@@ -19,6 +19,42 @@ class _NewPost extends State<NewPost> {
             fontSize: 20.0,
           ),
         ),
+        new Padding(padding: const EdgeInsets.symmetric(vertical: 10.0)),
+        new Text(
+          "Description",
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 15.0,
+          ),
+        ),
+        new SizedBox(
+            width: 300.0,
+            child: new TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Write something here',
+            ),
+          ),
+        ),
+        new Padding(padding: const EdgeInsets.symmetric(vertical: 15.0)),
+        new Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            new FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "POST",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 15.0,
+                  ),
+                ),
+            ),
+            new Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0)),
+
+          ],
+        ),
       ],
     );
   }
